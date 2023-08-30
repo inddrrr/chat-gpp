3790cd3758996e06bf08dce06e436be30b618ff2
Indra Bayu Pratama <indrabp2904@gmail.com>
2023-08-31 01:14:55 +0700

    minor

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-const { Client, LocalAuth } = require("whatsapp-web.js");
+const { Client, LocalAuth, MessageMedia } = require("whatsapp-web.js");
 require("dotenv").config();
 const qrcode = require("qrcode-terminal");
 const puppeteer = require("puppeteer");
-const PREFIX = process.env.PREFIX_BOT;
-const BOT_NAME = process.env.BOT_NAME;
-
 const OpenAI = require("openai");
+const GPP_PREFIX = process.env.GPP_PREFIX;
+const DRAW_PREFIX = process.env.DRAW_PREFIX;
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY // This is also the default, can be omitted
@@ -42,10 +41,73 @@ client.on("message", async msg => {
   } else if (msg.body === '.gmn') {
     // Send a new message to the same chat
     client.sendMessage(msg.from, 'gpp');
+
+  } else if (msg.body.startsWith(GPP_PREFIX) && !msg.getChat().isGroup) {
+    const message = msg.body.replace(`${GPP_PREFIX} `, "");
+
+    response = await gpp(message);
+    msg.reply(response);
+
+  } else if (msg.body.startsWith(DRAW_PREFIX) && !msg.getChat().isGroup) {
+    const message = msg.body.replace(`${DRAW_PREFIX} `, "");
+
+    response = await draw(message);
+    await client.sendMessage(msg.from, response, {caption: "nyoh!"});
   } 
 });
+
+async function gpp(message) {
+  try {
+    const response = await openai.completions.create({
+      model: "text-davinci-003",
+      prompt: message,
+      max_tokens: 3000,
+    });
+    console.log(response.choices[0].text);
+
+    return response.choices[0].text;
+
+  } catch (error) {
+    if (error instanceof OpenAI.APIError) {
+      console.error(error.status); // e.g. 401
+      console.error(error.message); // e.g. The authentication token you passed was invalid...
+      console.error(error.code); // e.g. 'invalid_api_key'
+      console.error(error.type);
+      return "Maaf, tolong ulangi.";
+    } else {
+      console.log(error);
+      return "Maaf, saya tidak mengerti maksud anda.";
+    }
+  }
+}
+
+async function draw(message) {
+  try {
+    const response = await openai.images.generate({
+      prompt: message,
+      n: 1,
+      size: "512x512",
+
+    })
+
+    const media = await MessageMedia.fromUrl(response.data[0].url);
+    return media;
+
+
+  } catch (error) {
+    if (error instanceof OpenAI.APIError) {
+      console.error(error.status); // e.g. 401
+      console.error(error.message); // e.g. The authentication token you passed was invalid...
+      console.error(error.code); // e.g. 'invalid_api_key'
+      console.error(error.type);
+      return "Maaf, tolong ulangi.";
+    } else {
+      console.log(error);
+      return "Maaf, saya tidak mengerti maksud anda.";
+    }
+  }
+}
 
 client.on("disconnected", (reason) => {
   console.log("disconnected chat-gpp", reason);
 });
-
